a1e46f1819b4b12a1ae93debb56ff41055d923ad
James Monger <jameskmonger@hotmail.co.uk>
2016-01-21 12:37:55 +0000

    Remove stubs from inversify.test.ts

--- test/inversify.test.ts
+++ test/inversify.test.ts
@@ -10,57 +10,6 @@ var expect = chai.expect;
 declare var Map;
 
 //******************************************************************************
-//* MOCKS AND STUBS
-//******************************************************************************
-interface FooInterface {
-  name : string;
-  greet() : string;
-}
-
-interface BarInterface {
-  name : string;
-  greet() : string;
-}
-
-interface FooBarInterface {
-  foo : FooInterface;
-  bar : BarInterface;
-  greet() : string;
-}
-
-export class Foo implements FooInterface {
-  public name : string;
-  constructor() {
-    this.name = "foo";
-  }
-  public greet() : string {
-    return this.name;
-  }
-}
-
-class Bar implements BarInterface {
-  public name : string;
-  constructor() {
-    this.name = "bar";
-  }
-  public greet() : string {
-    return this.name;
-  }
-}
-
-class FooBar implements FooBarInterface {
-  public foo : FooInterface;
-  public bar : BarInterface;
-  constructor(FooInterface : FooInterface, BarInterface : BarInterface) {
-    this.foo = FooInterface;
-    this.bar = BarInterface;
-  }
-  public greet() : string{
-    return this.foo.greet() + this.bar.greet();
-  }
-}
-
-//******************************************************************************
 //* TYPE BINDING CLASS
 //******************************************************************************
 describe("Type Binging Class Test Suite \n", () => {
