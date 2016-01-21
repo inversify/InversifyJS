a2df5e563b57ad862a2342112e8f24b65b8758c3
James Monger <jameskmonger@hotmail.co.uk>
2016-01-21 12:33:54 +0000

    Remove polyfill for bind in PhantomJS

--- test/inversify.test.ts
+++ test/inversify.test.ts
@@ -9,36 +9,6 @@ var expect = chai.expect;
 
 declare var Map;
 
-// Polyfill for Function.prototype.bind more details at
-// https://github.com/ariya/phantomjs/issues/10522
-if (!Function.prototype.bind) {
-  Function.prototype.bind = function(oThis) {
-    if (typeof this !== 'function') {
-      // closest thing possible to the ECMAScript 5
-      // internal IsCallable function
-      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
-    }
-
-    var aArgs   = Array.prototype.slice.call(arguments, 1),
-        fToBind = this,
-        fNOP    = function() {},
-        fBound  = function() {
-          return fToBind.apply(this instanceof fNOP
-                 ? this
-                 : oThis,
-                 aArgs.concat(Array.prototype.slice.call(arguments)));
-        };
-
-    if (this.prototype) {
-      // native functions don't have a prototype
-      fNOP.prototype = this.prototype;
-    }
-    fBound.prototype = new fNOP();
-
-    return fBound;
-  };
-}
-
 //******************************************************************************
 //* MOCKS AND STUBS
 //******************************************************************************
