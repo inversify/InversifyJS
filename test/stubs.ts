00a7872a7c0e67aabfe80d34b2731df9da946786
James Monger <jameskmonger@hotmail.co.uk>
2016-01-21 12:37:19 +0000

    Move stubs to stubs.ts

---
+++ test/stubs.ts
@@ -0,0 +1,47 @@
+interface FooInterface {
+  name : string;
+  greet() : string;
+}
+
+interface BarInterface {
+  name : string;
+  greet() : string;
+}
+
+interface FooBarInterface {
+  foo : FooInterface;
+  bar : BarInterface;
+  greet() : string;
+}
+
+export class Foo implements FooInterface {
+  public name : string;
+  constructor() {
+    this.name = "foo";
+  }
+  public greet() : string {
+    return this.name;
+  }
+}
+
+class Bar implements BarInterface {
+  public name : string;
+  constructor() {
+    this.name = "bar";
+  }
+  public greet() : string {
+    return this.name;
+  }
+}
+
+class FooBar implements FooBarInterface {
+  public foo : FooInterface;
+  public bar : BarInterface;
+  constructor(FooInterface : FooInterface, BarInterface : BarInterface) {
+    this.foo = FooInterface;
+    this.bar = BarInterface;
+  }
+  public greet() : string{
+    return this.foo.greet() + this.bar.greet();
+  }
+}
