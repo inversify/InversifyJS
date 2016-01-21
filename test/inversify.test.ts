9be97ea3c0ee2bd8451739fb012fa7c8b8ca9fa5
James Monger <jameskmonger@hotmail.co.uk>
2016-01-21 12:44:57 +0000

    Remove tests from main inversify test class

--- test/inversify.test.ts
+++ test/inversify.test.ts
@@ -12,52 +12,6 @@ var expect = chai.expect;
 declare var Map;
 
 //******************************************************************************
-//* TYPE BINDING CLASS
-//******************************************************************************
-describe("Type Binding Class Test Suite \n", () => {
-
-  it('It should set its own properties correctly \n', (done) => {
-
-    var runtimeIdentifier = "FooInterface";
-    var binding =  new TypeBinding<Stubs.FooInterface>(runtimeIdentifier, Stubs.Foo);
-    expect(binding.runtimeIdentifier).to.equals(runtimeIdentifier);
-    expect(binding.implementationType).to.not.equals(null);
-    expect(binding.cache).to.equals(null);
-    expect(binding.scope).to.equal(TypeBindingScopeEnum.Transient);
-
-    var runtimeIdentifier = "BarInterface";
-    var binding =  new TypeBinding<Stubs.BarInterface>(
-      runtimeIdentifier, Stubs.Bar, TypeBindingScopeEnum.Singleton);
-
-    expect(binding.runtimeIdentifier).to.equals(runtimeIdentifier);
-    expect(binding.implementationType).to.not.equals(null);
-    expect(binding.cache).to.equals(null);
-    expect(binding.scope).to.equal(TypeBindingScopeEnum.Singleton);
-
-    done();
-  });
-
-  it("It should be able to use implementationType as a constructor \n", (done) => {
-    var runtimeIdentifier = "FooInterface";
-    var binding =  new TypeBinding<Stubs.FooInterface>(runtimeIdentifier, Stubs.Foo);
-    var instance = new binding.implementationType();
-    expect(instance.greet()).to.equals("foo");
-    done();
-  });
-
-  it("Throws when invalid scope \n", (done) => {
-    var runtimeIdentifier = "FooInterface";
-    var scopeType = 3;
-    var fn = function() {
-      new TypeBinding<Stubs.FooInterface>(runtimeIdentifier, Stubs.Foo, scopeType);
-    }
-    expect(fn).to.throw(`Invalid scope type ${scopeType}`);
-    done();
-  });
-
-});
-
-//******************************************************************************
 //* KERNEL CLASS
 //******************************************************************************
 describe('Kernel Test Suite \n', () => {
