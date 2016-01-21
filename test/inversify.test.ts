94fc7051c64fd813159de16d7e7bec030b349a08
James Monger <jameskmonger@hotmail.co.uk>
2016-01-21 12:38:46 +0000

    Fix references in first describe

--- test/inversify.test.ts
+++ test/inversify.test.ts
@@ -19,15 +19,15 @@ describe("Type Binging Class Test Suite \n", () => {
   it('It should set its own properties correctly \n', (done) => {
 
     var runtimeIdentifier = "FooInterface";
-    var binding =  new TypeBinding<FooInterface>(runtimeIdentifier, Foo);
+    var binding =  new TypeBinding<Stubs.FooInterface>(runtimeIdentifier, Stubs.Foo);
     expect(binding.runtimeIdentifier).to.equals(runtimeIdentifier);
     expect(binding.implementationType).to.not.equals(null);
     expect(binding.cache).to.equals(null);
     expect(binding.scope).to.equal(TypeBindingScopeEnum.Transient);
 
     var runtimeIdentifier = "BarInterface";
-    var binding =  new TypeBinding<BarInterface>(
-      runtimeIdentifier, Bar, TypeBindingScopeEnum.Singleton);
+    var binding =  new TypeBinding<Stubs.BarInterface>(
+      runtimeIdentifier, Stubs.Bar, TypeBindingScopeEnum.Singleton);
 
     expect(binding.runtimeIdentifier).to.equals(runtimeIdentifier);
     expect(binding.implementationType).to.not.equals(null);
@@ -39,7 +39,7 @@ describe("Type Binging Class Test Suite \n", () => {
 
   it("It should be able to use implementationType as a constructor \n", (done) => {
     var runtimeIdentifier = "FooInterface";
-    var binding =  new TypeBinding<FooInterface>(runtimeIdentifier, Foo);
+    var binding =  new TypeBinding<Stubs.FooInterface>(runtimeIdentifier, Stubs.Foo);
     var instance = new binding.implementationType();
     expect(instance.greet()).to.equals("foo");
     done();
@@ -49,7 +49,7 @@ describe("Type Binging Class Test Suite \n", () => {
     var runtimeIdentifier = "FooInterface";
     var scopeType = 3;
     var fn = function() {
-      new TypeBinding<FooInterface>(runtimeIdentifier, Foo, scopeType);
+      new TypeBinding<Stubs.FooInterface>(runtimeIdentifier, Stubs.Foo, scopeType);
     }
     expect(fn).to.throw(`Invalid scope type ${scopeType}`);
     done();
