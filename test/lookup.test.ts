1bfb269a6b16924f9aa55baf0d02d9a587da2dd1
James Monger <jameskmonger@hotmail.co.uk>
2016-01-21 12:55:02 +0000

    Remove 'done' callbacks (unnecessary)

--- test/lookup.test.ts
+++ test/lookup.test.ts
@@ -3,40 +3,35 @@ import { Lookup } from "../source/lookup";
 
 describe('Lookup', () => {
 
-  it('key cannot be null when invoking get() remove() or hasKey()', (done) => {
+  it('key cannot be null when invoking get() remove() or hasKey()', () => {
     var lookup = new Lookup<any>();
     expect(() => { lookup.get(null); }).to.throw("Argument Null");
     expect(() => { lookup.remove(null); }).to.throw("Argument Null");
     expect(() => { lookup.hasKey(null); }).to.throw("Argument Null");
-    done();
   });
 
-  it('key cannot be null when invoking add()', (done) => {
+  it('key cannot be null when invoking add()', () => {
     var lookup = new Lookup<any>();
     expect(() => { lookup.add(null, 1); }).to.throw("Argument Null");
-    done();
   });
 
-  it('value cannot be null when invoking add()', (done) => {
+  it('value cannot be null when invoking add()', () => {
     var lookup = new Lookup<any>();
     expect(() => { lookup.add("TEST_KEY", null); }).to.throw("Argument Null");
-    done();
   });
 
-  it('value cannot be null when invoking add()', (done) => {
+  it('value cannot be null when invoking add()', () => {
     var lookup = new Lookup<any>();
     var key = "TEST_KEY";
     lookup.add(key, 1);
     lookup.add(key, 2);
     var result = lookup.get(key);
     expect(result.length).to.eql(2);
-    done();
   });
 
-  it('throws when key not found', (done) => {
+  it('throws when key not found', () => {
     var lookup = new Lookup<any>();
     expect(() => { lookup.get("THIS_KEY_IS_NOT_AVAILABLE"); }).to.throw("Key Not Found");
-    done();
   });
 
 });
