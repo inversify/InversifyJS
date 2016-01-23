f1931fe7f2e042fb2659622487a3e9b573bbae7e
remojansen <remo.jansen@wolksoftware.com>
2016-01-23 22:46:37 +0000

    fixed .d.ts issues and added check to build

--- type_definitions/inversify-global.d.ts
+++ type_definitions/inversify-global.d.ts
@@ -1,15 +1,57 @@
-// Type definitions for inversify 1.0.0
+// Type definitions for inversify 1.2.1
 // Project: https://github.com/inversify/InversifyJS
 // Definitions by: inversify <https://github.com/inversify>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-/// <reference path="inversify.d.ts"/>
+declare namespace inversify {
 
-import inversify = __inversify;
+  interface TypeBindingInterface<TServiceType> {
+    runtimeIdentifier : string;
+    implementationType : { new(): TServiceType ;};
+    cache : TServiceType;
+    scope : number; // TypeBindingScopeEnum
+  }
 
-declare namespace _inversify {
-    export import Kernel = __inversify.Kernel;
-    export import TypeBinding = __inversify.TypeBinding;
-    export import Inject = __inversify.Inject;
-    export import TypeBindingScopeEnum = __inversify.TypeBindingScopeEnum;
+  interface KernelInterface {
+    bind(typeBinding : TypeBindingInterface<any>) : void;
+    unbind(runtimeIdentifier : string) : void;
+    unbindAll() : void;
+    resolve<TImplementationType>(runtimeIdentifier : string) : TImplementationType;
+  }
+
+  export enum TypeBindingScopeEnum {
+      Transient = 0,
+      Singleton = 1,
+  }
+
+  export class TypeBinding<TServiceType> implements TypeBindingInterface<TServiceType> {
+      runtimeIdentifier: string;
+      implementationType: {
+          new (): TServiceType;
+      };
+      cache: TServiceType;
+      scope: TypeBindingScopeEnum;
+      constructor(runtimeIdentifier: string, implementationType: {
+          new (...args: any[]): TServiceType;
+      }, scopeType?: TypeBindingScopeEnum);
+  }
+
+  export class Kernel implements KernelInterface {
+      private _bindings;
+      bind(typeBinding: TypeBindingInterface<any>): void;
+      unbind(runtimeIdentifier: string): void;
+      unbindAll(): void;
+      resolve<TImplementationType>(runtimeIdentifier: string): TImplementationType;
+      private _validateBinding(typeBinding);
+      private _getConstructorArguments(func);
+      private _injectDependencies<TImplementationType>(func);
+      private _construct<TImplementationType>(constr, args);
+      constructor();
+  }
+
+  interface InjectableConstructorInterface {
+      argumentTypes: Array<string>;
+  }
+
+  function Inject(typeIdentifier: string): (typeConstructor: InjectableConstructorInterface, propertyName: string, argumentIndex: number) => void;
 }
