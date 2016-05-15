e394e00f6257a9f525facb5997397eff234be6c8
Benjamin Otto <benjamin.otto@style-systems.de>
2016-05-15 20:02:00 +0200

    removed global injection map (#203)
    use Reflect metadata API :)

--- src/annotation/property_injectors.ts
+++ src/annotation/property_injectors.ts
@@ -1,47 +1,22 @@
 ///<reference path="../interfaces/interfaces.d.ts" />
 
+const INJECTION = Symbol();
+
 function _proxyGetter(
-    kernel: any,
-    serviceIdentifier: (string|Symbol|INewable<any>),
     proto: any,
     key: string,
-    resolve: (kernel: IKernel, serviceIdentifier: (string|Symbol|INewable<any>)) => any
+    resolve: () => any
 ) {
-
-    let globalInjectionMap: WeakMap<any, any> = kernel._injectedProperties;
-
-    let getter = function () {
-
-        let instanceInjections: Map<string, any> = globalInjectionMap.get(this);
-
-        if (instanceInjections == null) {
-            instanceInjections = new Map();
-            globalInjectionMap.set(this, instanceInjections);
+    function getter() {
+        if (!Reflect.hasMetadata(INJECTION, this, key)) {
+            Reflect.defineMetadata(INJECTION, resolve(), this, key);
         }
+        return Reflect.getMetadata(INJECTION, this, key);
+    }
 
-        if (!instanceInjections.has(key)) {
-            let val = resolve(kernel, serviceIdentifier);
-            instanceInjections.set(key, val);
-        }
-
-        return instanceInjections.get(key);
-
-    };
-
-    let  setter = function(newVal: any) {
-
-        let instanceInjections: Map<string, any> = globalInjectionMap.get(this);
-
-        if (instanceInjections == null) {
-            instanceInjections = new Map();
-            globalInjectionMap.set(this, instanceInjections);
-        }
-
-        instanceInjections.set(key, newVal);
-
-    };
-
-    delete proto[key];
+    function setter(newVal: any) {
+        Reflect.defineMetadata(INJECTION, newVal, this, key);
+    }
 
     Object.defineProperty(proto, key, {
         configurable: true,
@@ -49,18 +24,17 @@ function _proxyGetter(
         get: getter,
         set: setter
     });
-
 }
 
 function makePropertyInjectDecorator(kernel: IKernel) {
     return function(serviceIdentifier: (string|Symbol|INewable<any>)) {
         return function(proto: any, key: string): void {
 
-            let resolve = (krln: IKernel, srvId: (string|Symbol|INewable<any>)) => {
-                return krln.get(srvId);
+            let resolve = () => {
+                return kernel.get(serviceIdentifier);
             };
 
-            _proxyGetter(kernel, serviceIdentifier, proto, key, resolve);
+            _proxyGetter(proto, key, resolve);
 
         };
     };
@@ -70,11 +44,11 @@ function makePropertyInjectNamedDecorator(kernel: IKernel) {
     return function(serviceIdentifier: (string|Symbol|INewable<any>), named: string) {
         return function(proto: any, key: string): void {
 
-            let resolve = (krln: IKernel, srvId: (string|Symbol|INewable<any>)) => {
-                return krln.getNamed(srvId, named);
+            let resolve = () => {
+                return kernel.getNamed(serviceIdentifier, named);
             };
 
-            _proxyGetter(kernel, serviceIdentifier, proto, key, resolve);
+            _proxyGetter(proto, key, resolve);
 
         };
     };
@@ -84,11 +58,11 @@ function makePropertyInjectTaggedDecorator(kernel: IKernel) {
     return function(serviceIdentifier: (string|Symbol|INewable<any>), key: string, value: any) {
         return function(proto: any, propertyName: string): void {
 
-            let resolve = (krln: IKernel, srvId: (string|Symbol|INewable<any>)) => {
-                return krln.getTagged(srvId, key, value);
+            let resolve = () => {
+                return kernel.getTagged(serviceIdentifier, key, value);
             };
 
-            _proxyGetter(kernel, serviceIdentifier, proto, propertyName, resolve);
+            _proxyGetter(proto, propertyName , resolve);
 
         };
     };
@@ -98,11 +72,11 @@ function makePropertyMultiInjectDecorator(kernel: IKernel) {
     return function(serviceIdentifier: (string|Symbol|INewable<any>)) {
         return function(proto: any, key: string): void {
 
-            let resolve = (krln: IKernel, srvId: (string|Symbol|INewable<any>)) => {
-                return krln.getAll(srvId);
+            let resolve = () => {
+                return kernel.getAll(serviceIdentifier);
             };
 
-            _proxyGetter(kernel, serviceIdentifier, proto, key, resolve);
+            _proxyGetter(proto, key, resolve);
 
         };
     };
