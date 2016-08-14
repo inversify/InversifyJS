7c619d0daeda9ff1a075ac5c238d2b113fb4fe73
remojansen <remo.jansen@wolksoftware.com>
2016-08-14 22:28:25 +0100

    [WIP] #310

--- src/annotation/inject.ts
+++ src/annotation/inject.ts
@@ -1,12 +1,19 @@
 import Metadata from "../planning/metadata";
-import { tagParameter } from "./decorator_utils";
+import { tagParameter, tagProperty } from "./decorator_utils";
 import * as METADATA_KEY from "../constants/metadata_keys";
 
 // Used to declare types to be injected not available at runtime
 function inject(serviceIdentifier: (string|Symbol)) {
-  return function(target: any, targetKey: string, index: number) {
+  return function(target: any, targetKey: string, index?: number) {
+
     let metadata = new Metadata(METADATA_KEY.INJECT_TAG, serviceIdentifier);
-    return tagParameter(target, targetKey, index, metadata);
+
+    if (index !== undefined) {
+      return tagParameter(target, targetKey, index, metadata);
+    } else {
+      return tagProperty(target, targetKey, metadata);
+    }
+
   };
 }
 
--- test/inversify.test.ts
+++ test/inversify.test.ts
@@ -2842,5 +2842,125 @@ describe("InversifyJS", () => {
                 expect(error.message).to.match(/.*\bShuriken\b.*\btagged\b.*\bcanThrow\b.*\btrue\b/);
             }
         });
+
+    });
+
+    describe("Property Injection", () => {
+
+        it("Should be able to inject a property", () => {
+
+            interface Weapon {
+                name: string;
+            }
+
+            @injectable()
+            class Katana implements Weapon {
+                public name: string;
+                public constructor() {
+                    this.name = "Katana";
+                }
+            }
+
+            interface Warrior {
+                name: string;
+                weapon: Weapon;
+            }
+
+            @injectable()
+            class Samurai implements Warrior {
+                public name: string;
+                @inject(TYPES.Weapon)
+                public weapon: Weapon;
+                public constructor() {
+                    this.name = "Samurai";
+                }
+            }
+
+            let TYPES = {
+                Warrior: "Warrior",
+                Weapon: "Weapon"
+            };
+
+            let kernel = new Kernel();
+            kernel.bind<Warrior>(TYPES.Warrior).to(Samurai);
+            kernel.bind<Weapon>(TYPES.Weapon).to(Katana);
+
+            let warrior = kernel.get<Warrior>(TYPES.Warrior);
+            expect(warrior.name).to.eql("Samurai");
+            expect(warrior.weapon).to.eql("Katana");
+
+        });
+
+        it("Should be able to inject a property combined with constructor injection", () => {
+
+            let TYPES = {
+                Warrior: "Warrior",
+                Weapon: "Weapon"
+            };
+
+            let TAGS = {
+                Primary: "Primary",
+                Secondary: "Secondary"
+            };
+
+            interface Weapon {
+                name: string;
+            }
+
+            @injectable()
+            class Katana implements Weapon {
+                public name: string;
+                public constructor() {
+                    this.name = "Katana";
+                }
+            }
+
+            @injectable()
+            class Shuriken implements Weapon {
+                public name: string;
+                public constructor() {
+                    this.name = "Shuriken";
+                }
+            }
+
+            interface Warrior {
+                name: string;
+                primaryWeapon: Weapon;
+                secondaryWeapon: Weapon;
+            }
+
+            @injectable()
+            class Samurai implements Warrior {
+
+                public name: string;
+                public primaryWeapon: Weapon;
+
+                @inject(TYPES.Weapon)
+                @named(TAGS.Secondary)
+                public secondaryWeapon: Weapon;
+
+                public constructor(
+                    @inject(TYPES.Weapon) @named(TAGS.Primary) weapon: Weapon
+                ) {
+                    this.name = "Samurai";
+                    this.primaryWeapon = weapon;
+                }
+            }
+
+            let kernel = new Kernel();
+            kernel.bind<Warrior>(TYPES.Warrior).to(Samurai);
+            kernel.bind<Weapon>(TYPES.Weapon).to(Katana).whenTargetNamed(TAGS.Primary);
+            kernel.bind<Weapon>(TYPES.Weapon).to(Katana).whenTargetNamed(TAGS.Secondary);
+
+            let warrior = kernel.get<Warrior>(TYPES.Warrior);
+            expect(warrior.name).to.eql("Samurai");
+            expect(warrior.primaryWeapon).to.eql("Katana");
+            expect(warrior.secondaryWeapon).to.eql("Shuriken");
+
+        });
+
+        it("Should be able to inject a property in a base class");
+        it("Should be able to inject a property using contextual constrains");
+
     });
 });
