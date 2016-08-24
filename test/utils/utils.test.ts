741ea4eaaa1a9567d79212d7e3395a7a18aae359
Remo H. Jansen <remo.jansen@wolksoftware.com>
2016-08-24 13:55:12 +0100

    Fixed #345 (#347)

--- test/utils/utils.test.ts
+++ test/utils/utils.test.ts
@@ -2,23 +2,26 @@ import { expect } from "chai";
 import { getFunctionName } from "../../src/utils/utils";
 
 describe("Utils", () => {
+
     it("Should return a good function name", () => {
+
         function testFunction() {
             return false;
         }
 
         expect(getFunctionName(testFunction)).eql("testFunction");
+
     });
 
     it("Should return a good function name by using the regex", () => {
+
         const testFunction: any = { name: null };
         testFunction.toString = () => {
             return "function testFunction";
         };
 
-        console.log(getFunctionName(testFunction));
-
         expect(getFunctionName(testFunction)).eql("testFunction");
+
     });
 
 });
