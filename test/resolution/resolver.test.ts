67c4bcf89f766a5c23f90f3e4a0a2488c710b7b2
Remo H. Jansen <remo.jansen@wolksoftware.com>
2016-08-29 22:55:27 +0100

    Update resolver.test.ts

--- test/resolution/resolver.test.ts
+++ test/resolution/resolver.test.ts
@@ -446,7 +446,7 @@ describe("Resolver", () => {
 
     let kernel = new Kernel();
     kernel.bind<UseDate>("UseDate").to(UseDate);
-    kernel.bind<Date>("Date").toDynamicValue(() => { return new Date(); });
+    kernel.bind<Date>("Date").toDynamicValue((context: interfaces.Context) => { return new Date(); });
 
     let subject1 = kernel.get<UseDate>("UseDate");
     let subject2 = kernel.get<UseDate>("UseDate");
