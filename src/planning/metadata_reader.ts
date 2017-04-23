93f83c2bcfbb06e2da7fa564c09d9f4ce77a8d38
Remo H. Jansen <remo.jansen@wolksoftware.com>
2017-04-23 21:27:23 +0100

    Fixes typo in public API #536 (#539)

--- src/planning/metadata_reader.ts
+++ src/planning/metadata_reader.ts
@@ -3,7 +3,7 @@ import * as METADATA_KEY from "../constants/metadata_keys";
 
 class MetadataReader implements interfaces.MetadataReader {
 
-    public getConstrucotorMetadata(constructorFunc: Function): interfaces.ConstructorMetadata {
+    public getConstructorMetadata(constructorFunc: Function): interfaces.ConstructorMetadata {
 
         // TypeScript compiler generated annotations
         let compilerGeneratedMetadata = Reflect.getMetadata(METADATA_KEY.PARAM_TYPES, constructorFunc);
