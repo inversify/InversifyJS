eebc10b9574eebdf76adcfc79c2b0d20ecde68f9
remojansen <remo.jansen@wolksoftware.com>
2017-04-25 21:06:13 +0100

    Expose metadata reader

--- test/features/metadata_reader.test.ts
+++ test/features/metadata_reader.test.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { Container } from "../../src/inversify";
+import { Container, injectable, inject, MetadataReader } from "../../src/inversify";
 import { interfaces } from "../../src/interfaces/interfaces";
 import { Metadata } from "../../src/planning/metadata";
 import * as METADATA_KEY from "../../src/constants/metadata_keys";
@@ -207,6 +207,110 @@ describe("Custom Metadata Reader", () => {
 
         expect(ninja.fight()).eql("cut!");
         expect(ninja.sneak()).eql("hit!");
+
+    });
+
+
+    it("Should be able to use extend the default metadata reader", () => {
+
+        const constructorMetadataLog: interfaces.ConstructorMetadata[] = [];
+        const propertyMetadataLog: interfaces.MetadataMap[] = [];
+
+        class CustomMetadataReader extends MetadataReader {
+            public getConstructorMetadata(constructorFunc: Function): interfaces.ConstructorMetadata {
+                const constructorMetadata = super.getConstructorMetadata(constructorFunc);
+                constructorMetadataLog.push(constructorMetadata);
+                return constructorMetadata;
+            }
+            public getPropertiesMetadata(constructorFunc: Function): interfaces.MetadataMap {
+                const propertyMetadata = super.getPropertiesMetadata(constructorFunc);
+                propertyMetadataLog.push(propertyMetadata);
+                return propertyMetadata;
+            }
+        }
+
+        interface Ninja {
+            fight(): string;
+            sneak(): string;
+        }
+
+        interface Katana {
+            hit(): string;
+        }
+
+        interface Shuriken {
+            throw(): string;
+        }
+
+        @injectable()
+        class Katana implements Katana {
+            public hit() {
+                return "cut!";
+            }
+        }
+
+        @injectable()
+        class Shuriken implements Shuriken {
+            public throw() {
+                return "hit!";
+            }
+        }
+
+        @injectable()
+        class Ninja implements Ninja {
+
+            private _katana: Katana;
+            private _shuriken: Shuriken;
+
+            public constructor(
+                @inject("Katana") katana: Katana,
+                @inject("Shuriken") shuriken: Shuriken
+            ) {
+                this._katana = katana;
+                this._shuriken = shuriken;
+            }
+
+            public fight() {return this._katana.hit(); }
+            public sneak() { return this._shuriken.throw(); }
+
+        }
+
+        let container = new Container();
+        container.applyCustomMetadataReader(new CustomMetadataReader());
+
+        container.bind<Ninja>("Ninja").to(Ninja);
+        container.bind<Katana>("Katana").to(Katana);
+        container.bind<Shuriken>("Shuriken").to(Shuriken);
+
+        let ninja = container.get<Ninja>("Ninja");
+
+        expect(ninja.fight()).eql("cut!");
+        expect(ninja.sneak()).eql("hit!");
+
+        expect(Array.isArray(constructorMetadataLog)).eq(true);
+        expect(constructorMetadataLog.length).eq(3);
+
+        let compilerGeneratedMetadata0 = constructorMetadataLog[0].compilerGeneratedMetadata;
+
+        if (compilerGeneratedMetadata0) {
+            expect(compilerGeneratedMetadata0[0]).eq(Katana);
+            expect(compilerGeneratedMetadata0[1]).eq(Shuriken);
+        }
+
+        expect(constructorMetadataLog[0].userGeneratedMetadata["0"][0].key).eq("inject");
+        expect(constructorMetadataLog[0].userGeneratedMetadata["0"][0].value).eq("Katana");
+        expect(constructorMetadataLog[0].userGeneratedMetadata["1"][0].key).eq("inject");
+        expect(constructorMetadataLog[0].userGeneratedMetadata["1"][0].value).eq("Shuriken");
+
+        expect(JSON.stringify(constructorMetadataLog[1].compilerGeneratedMetadata)).eq(JSON.stringify([]));
+        expect(JSON.stringify(constructorMetadataLog[2].compilerGeneratedMetadata)).eq(JSON.stringify([]));
+        expect(JSON.stringify(constructorMetadataLog[1].userGeneratedMetadata)).eq(JSON.stringify({}));
+        expect(JSON.stringify(constructorMetadataLog[2].userGeneratedMetadata)).eq(JSON.stringify({}));
+
+        expect(propertyMetadataLog.length).eq(3);
+        expect(propertyMetadataLog[0].length).eq(0);
+        expect(propertyMetadataLog[1].length).eq(0);
+        expect(propertyMetadataLog[2].length).eq(0);
 
     });
 
