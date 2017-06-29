dff05d4baca3145b6f435e699612e9906666c1b3
Dan Cavanagh <djcavanagh@gmail.com>
2017-06-29 03:50:28 -0400

    VS Code mocha debugging  (#589)
    * added launch.json to allow debugging mocha in vscode
    changed gulp sourcemaps output to match tsc output
    added *.js.map to gitignore
    removed .vscode/launch.json from gitignore
    
    * Added source mapping for debugging
    
    * hide js.map

--- gulpfile.js
+++ gulpfile.js
@@ -128,7 +128,11 @@ gulp.task("build-src", function() {
     .on("error", function (err) {
         process.exit(1);
     })
-    .js.pipe(sourcemaps.write())
+    .js.pipe(sourcemaps.write(".", {
+        sourceRoot: function(file) {
+            return file.cwd + '/src';
+        }
+    }))
     .pipe(gulp.dest("src/"));
 });
 
@@ -143,7 +147,11 @@ gulp.task("build-test", function() {
     .on("error", function (err) {
         process.exit(1);
     })
-    .js.pipe(sourcemaps.write())
+    .js.pipe(sourcemaps.write(".", {
+        sourceRoot: function(file) {
+            return file.cwd + '/test';
+        }
+    }))
     .pipe(gulp.dest("test/"));
 });
 
