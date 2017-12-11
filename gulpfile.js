344f378096d93d7bcb34fd4aea46138c388159b9
Remo H. Jansen <remo.jansen@wolksoftware.com>
2017-12-11 10:56:19 +0000

    Prevents green builds with failing tests (#710)
    e.g. https://travis-ci.org/inversify/InversifyJS/jobs/314689360#L2304-L2312

--- gulpfile.js
+++ gulpfile.js
@@ -161,6 +161,9 @@ gulp.task("mocha", [ "istanbul:hook" ], function() {
       "test/**/*.test.js"
     ])
     .pipe(mocha({ui: "bdd"}))
+    .on("error", function (err) {
+        process.exit(1);
+    })
     .pipe(istanbul.writeReports());
 });
 
