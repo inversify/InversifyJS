414f79bc245c0e9295403f4cf5e1b3c00a4ab0b0
Dan <djcavanagh@gmail.com>
2019-06-01 11:41:36 -0400

    updated dependencies updated gulpfile to work with gulp 4 set tslint to recomended fixed some tslint errors
    need to fix tests

--- gulpfile.js
+++ gulpfile.js
@@ -158,7 +158,14 @@ gulp.task("build-test", function () {
         .pipe(gulp.dest("test/"));
 });
 
-gulp.task("mocha", ["istanbul:hook"], function () {
+gulp.task("istanbul:hook", function () {
+    return gulp.src(["src/**/*.js"])
+        .pipe(istanbul())
+        .pipe(sourcemaps.write("."))
+        .pipe(istanbul.hookRequire());
+});
+
+gulp.task("mocha", gulp.series("istanbul:hook"), function () {
     return gulp.src([
         "node_modules/reflect-metadata/Reflect.js",
         "test/**/*.test.js"
@@ -169,14 +176,10 @@ gulp.task("mocha", ["istanbul:hook"], function () {
             process.exit(1);
         })
         .pipe(istanbul.writeReports());
+
 });
 
-gulp.task("istanbul:hook", function () {
-    return gulp.src(["src/**/*.js"])
-        .pipe(istanbul())
-        .pipe(sourcemaps.write("."))
-        .pipe(istanbul.hookRequire());
-});
+
 
 //******************************************************************************
 //* TESTS BROWSER
@@ -201,7 +204,7 @@ gulp.task("bundle-test", function () {
         .pipe(gulp.dest(outputFolder));
 });
 
-gulp.task("karma", ["bundle-test"], function (done) {
+gulp.task("karma", gulp.series("bundle-test"), function (done) {
     new karma.Server({
         configFile: __dirname + "/karma.conf.js"
     }, function (code) {
@@ -217,36 +220,19 @@ gulp.task("karma", ["bundle-test"], function (done) {
 
 // Run browser testings on AppVeyor not in Travis CI
 if (process.env.APPVEYOR) {
-    gulp.task("test", function (cb) {
-        runSequence("mocha", "karma", cb);
-    });
+    gulp.task("test", gulp.series("mocha", "karma", cb));
 } else {
-    gulp.task("test", function (cb) {
-        runSequence("mocha", cb);
-    });
+    gulp.task("test", gulp.series("mocha"));
 }
 
 //******************************************************************************
 //* DEFAULT
 //******************************************************************************
-gulp.task("build", function (cb) {
-    runSequence(
-        "lint",
-        [
-            "build-src",
-            "build-es",
-            "build-lib",
-            "build-amd",
-            "build-dts"
-        ],
-        "build-test", cb
-    );
-});
+gulp.task("build", gulp.series("lint", gulp.parallel(
+    "build-src",
+    "build-es",
+    "build-lib",
+    "build-amd",
+    "build-dts"), "build-test"))
 
-gulp.task("default", function (cb) {
-    runSequence(
-        "clean",
-        "build",
-        "test",
-        cb);
-});
+gulp.task("default", gulp.series("clean", "build", "test"));
