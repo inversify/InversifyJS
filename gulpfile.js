4597dd299f8b935c28b4a1070d0c0eef83e4e85e
Paris Holley <mail@parisholley.com>
2019-08-22 23:10:04 -0400

    isn't the tests, either clean or build

--- gulpfile.js
+++ gulpfile.js
@@ -222,7 +222,7 @@ gulp.task("karma", gulp.series("bundle-test", function (done) {
 
 // Run browser testings on AppVeyor not in Travis CI
 if (process.env.APPVEYOR) {
-    gulp.task("test", gulp.series("mocha"));
+    gulp.task("test", gulp.series("mocha", "karma"));
 } else {
     gulp.task("test", gulp.series("mocha"));
 }
