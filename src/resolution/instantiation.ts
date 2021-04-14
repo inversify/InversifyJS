b792336932730c3b8de53e7b231a481271776a2c
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2021-04-14 10:24:18 +0200

    refactor: update instantiation with better typings

--- src/resolution/instantiation.ts
+++ src/resolution/instantiation.ts
@@ -5,32 +5,32 @@ import { interfaces } from "../interfaces/interfaces";
 import { Metadata } from "../planning/metadata";
 import { isPromise } from "../utils/async";
 
-function _injectProperties(
-    instance: any,
+function _injectProperties<T>(
+    instance: T,
     childRequests: interfaces.Request[],
     resolveRequest: interfaces.ResolveRequestHandler
-): any {
+): T {
     const propertyInjectionsRequests = _filterRequestsByTargetType(childRequests, TargetTypeEnum.ClassProperty);
 
     const propertyInjections = propertyInjectionsRequests.map(resolveRequest);
 
     propertyInjectionsRequests.forEach((r: interfaces.Request, index: number) => {
-        let propertyName = "";
-        propertyName = r.target.name.value();
+        const propertyName = r.target.name.value();
         const injection = propertyInjections[index];
-        instance[propertyName] = injection;
+
+        (instance as Record<string, unknown>)[propertyName] = injection;
     });
 
     return instance;
 
 }
 
-function _createInstance(
-    constr: interfaces.Newable<any>,
+function _createInstance<T>(
+    constr: interfaces.Newable<T>,
     childRequests: interfaces.Request[],
     resolveRequest: interfaces.ResolveRequestHandler,
-): any {
-    let result: any;
+): T | Promise<T> {
+    let result: T | Promise<T>;
 
     if (childRequests.length > 0) {
         const constructorInjections = _getConstructionInjections(childRequests, resolveRequest);
@@ -52,13 +52,13 @@ function _createInstance(
     return result;
 }
 
-function _createInstanceWithConstructorInjections(
-    constructorInjections: any[],
-    constr: interfaces.Newable<any>,
+function _createInstanceWithConstructorInjections<T>(
+    constructorInjections: unknown[],
+    constr: interfaces.Newable<T>,
     childRequests: interfaces.Request[],
     resolveRequest: interfaces.ResolveRequestHandler,
-): any {
-    let result: any;
+): T {
+    let result: T;
 
     result = new constr(...constructorInjections);
     result = _injectProperties(result, childRequests, resolveRequest);
@@ -66,12 +66,12 @@ function _createInstanceWithConstructorInjections(
     return result;
 }
 
-async function _createInstanceWithConstructorInjectionsAsync(
-    constructorInjections: (any | Promise<any>)[],
-    constr: interfaces.Newable<any>,
+async function _createInstanceWithConstructorInjectionsAsync<T>(
+    constructorInjections: (unknown | Promise<unknown>)[],
+    constr: interfaces.Newable<T>,
     childRequests: interfaces.Request[],
     resolveRequest: interfaces.ResolveRequestHandler,
-): Promise<any> {
+): Promise<T> {
     return _createInstanceWithConstructorInjections(
         await Promise.all(constructorInjections),
         constr,
@@ -85,13 +85,13 @@ function _filterRequestsByTargetType(requests: interfaces.Request[], type: inter
         (request.target !== null && request.target.type === type));
 }
 
-function _getConstructionInjections(childRequests: interfaces.Request[], resolveRequest: interfaces.ResolveRequestHandler): any[] {
+function _getConstructionInjections(childRequests: interfaces.Request[], resolveRequest: interfaces.ResolveRequestHandler): unknown[] {
     const constructorInjectionsRequests = _filterRequestsByTargetType(childRequests, TargetTypeEnum.ConstructorArgument);
 
     return constructorInjectionsRequests.map(resolveRequest);
 }
 
-function _getInstanceAfterPostConstruct(constr: interfaces.Newable<any>, result: any): any {
+function _getInstanceAfterPostConstruct<T>(constr: interfaces.Newable<T>, result: T): T | Promise<T> {
 
     const postConstructResult = _postConstruct(constr, result);
 
@@ -102,18 +102,18 @@ function _getInstanceAfterPostConstruct(constr: interfaces.Newable<any>, result:
     }
 }
 
-function _postConstruct(constr: interfaces.Newable<any>, result: any): void | Promise<void> {
+function _postConstruct<T>(constr: interfaces.Newable<T>, instance: T): void | Promise<void> {
     if (Reflect.hasMetadata(METADATA_KEY.POST_CONSTRUCT, constr)) {
         const data: Metadata = Reflect.getMetadata(METADATA_KEY.POST_CONSTRUCT, constr);
         try {
-            return result[data.value]();
+            return (instance as T & Record<string, () => void>)[data.value]();
         } catch (e) {
             throw new Error(POST_CONSTRUCT_ERROR(constr.name, e.message));
         }
     }
 }
 
-function _validateInstanceResolution(binding: interfaces.Binding<any>, constr: interfaces.Newable<any>): void {
+function _validateInstanceResolution(binding: interfaces.Binding<unknown>, constr: interfaces.Newable<unknown>): void {
     if (binding.scope === "Transient") {
         if (typeof binding.onDeactivation === "function") {
             throw new Error(ON_DEACTIVATION_ERROR(constr.name, "Class cannot be instantiated in transient scope."));
@@ -125,12 +125,12 @@ function _validateInstanceResolution(binding: interfaces.Binding<any>, constr: i
     }
 }
 
-function resolveInstance(
-    binding: interfaces.Binding<any>,
-    constr: interfaces.Newable<any>,
+function resolveInstance<T>(
+    binding: interfaces.Binding<T>,
+    constr: interfaces.Newable<T>,
     childRequests: interfaces.Request[],
     resolveRequest: interfaces.ResolveRequestHandler,
-): any {
+): T | Promise<T> {
     _validateInstanceResolution(binding, constr);
 
     const result = _createInstance(constr, childRequests, resolveRequest);
