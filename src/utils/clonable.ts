a3b7c9d68a1622f6afdcd74fa5620e76fe9f4dca
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2021-04-14 10:29:45 +0200

    refactor: update Lookup.clone to use a Typeguard

---
+++ src/utils/clonable.ts
@@ -0,0 +1,10 @@
+import { interfaces } from "../interfaces/interfaces";
+
+function isClonable<T>(obj: unknown): obj is interfaces.Clonable<T> {
+    return (typeof obj === 'object')
+        && (obj !== null)
+        && ('clone' in obj)
+        && typeof (obj as interfaces.Clonable<T>).clone === 'function';
+}
+
+export { isClonable };
