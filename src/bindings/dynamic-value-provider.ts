b492db359eb217ce580b684566bfe306b5ee6148
Tony Hallett <tonyhallett74@gmail.com>
2021-05-04 14:09:15 +0100

    correct signatures

--- src/bindings/dynamic-value-provider.ts
+++ src/bindings/dynamic-value-provider.ts
@@ -1,7 +1,7 @@
 import { interfaces } from "../interfaces/interfaces";
 export class DynamicValueProvider<TActivated> implements interfaces.DynamicValueProvider<TActivated>{
   valueFrom: interfaces.DynamicValue<TActivated>;
-  provideValue(): TActivated|Promise<TActivated>{
-      return this.valueFrom(null as any);
+  provideValue(context:interfaces.Context, _:interfaces.Request[]): TActivated|Promise<TActivated>{
+      return this.valueFrom(context);
   }
 }
--- src/bindings/instance-value-provider.ts
+++ src/bindings/instance-value-provider.ts
@@ -2,7 +2,7 @@ import { interfaces } from "../interfaces/interfaces";
 
 export class InstanceValueProvider<TActivated> implements interfaces.InstanceValueProvider<TActivated>{
   valueFrom: interfaces.Newable<TActivated>;
-  provideValue(): TActivated {
+  provideValue(context:interfaces.Context, _:interfaces.Request[]): TActivated {
       throw new Error("Not implemented");
   }
 }
