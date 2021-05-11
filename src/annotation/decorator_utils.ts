b7b77416bbd4129881b1e3f416e87175ee1457a8
Tony Hallett <tonyhallett74@gmail.com>
2021-05-11 12:15:26 +0100

    DecoratorTarget for js decorate

--- src/annotation/decorator_utils.ts
+++ src/annotation/decorator_utils.ts
@@ -104,19 +104,19 @@ function _param(paramIndex: number, decorator: ParameterDecorator) {
 }
 
 // Allows VanillaJS developers to use decorators:
-// decorate(injectable("Foo", "Bar"), FooBar);
+// decorate(injectable(), FooBar);
 // decorate(targetName("foo", "bar"), FooBar);
 // decorate(named("foo"), FooBar, 0);
 // decorate(tagged("bar"), FooBar, 1);
 function decorate(
     decorator: (ClassDecorator | ParameterDecorator | MethodDecorator),
-    target: any,
-    parameterIndex?: number | string): void {
+    target: DecoratorTarget,
+    parameterIndexOrProperty?: number | string): void {
 
-    if (typeof parameterIndex === "number") {
-        _decorate([_param(parameterIndex, decorator as ParameterDecorator)], target);
-    } else if (typeof parameterIndex === "string") {
-        Reflect.decorate([decorator as MethodDecorator], target, parameterIndex);
+    if (typeof parameterIndexOrProperty === "number") {
+        _decorate([_param(parameterIndexOrProperty, decorator as ParameterDecorator)], target);
+    } else if (typeof parameterIndexOrProperty === "string") {
+        Reflect.decorate([decorator as MethodDecorator], target, parameterIndexOrProperty);
     } else {
         _decorate([decorator as ClassDecorator], target);
     }
