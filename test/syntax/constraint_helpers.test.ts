9996197d1b83f49a9661682da004757edfb7e916
Luiz Guilherme D'Abruzzo Pereira <luiz290788@gmail.com>
2021-05-16 11:58:52 +0000

    add tests for multiTaggedConstraint

--- test/syntax/constraint_helpers.test.ts
+++ test/syntax/constraint_helpers.test.ts
@@ -1,5 +1,7 @@
 import { expect } from "chai";
-import { typeConstraint } from "../../src/syntax/constraint_helpers";
+import { Container, injectable, MetadataReader, TargetTypeEnum } from "../../src/inversify";
+import { plan } from "../../src/planning/planner";
+import { multiTaggedConstraint, typeConstraint } from "../../src/syntax/constraint_helpers";
 
 describe("BindingInSyntax", () => {
 
@@ -10,4 +12,52 @@ describe("BindingInSyntax", () => {
 
     });
 
+    describe("multiTaggedConstraint", () => {
+
+        interface Weapon {
+            use(): string;
+        }
+
+        @injectable()
+        class Katana implements Weapon {
+            public use() {
+                return "katana!";
+            }
+        }
+
+        @injectable()
+        class Shuriken implements Weapon {
+            constructor(private readonly spikes: number) { }
+            public use() {
+                return `${this.spikes} spikes shuriken!`;
+            }
+        }
+
+        const container = new Container();
+        container.bind<Weapon>("Weapon").toConstantValue(new Shuriken(3))
+            .whenTargetMultiTagged(['throwable', true], ["spikes", 3]);
+        container.bind<Weapon>("Weapon").toConstantValue(new Shuriken(4))
+            .whenTargetMultiTagged(['throwable', true], ["spikes", 4]);
+        container.bind<Weapon>("Weapon").to(Katana).whenTargetTagged("throwable", false);
+
+        it("should return true when all tags are present", () => {
+            const context = plan(new MetadataReader(), container, false, TargetTypeEnum.Variable,
+                "Weapon", [["throwable", false], ["spikes", 4]])
+
+            const constraint = multiTaggedConstraint(["throwable", false], ["spikes", 4]);
+            const constraintResult = constraint(context.plan.rootRequest)
+
+            expect(constraintResult).to.equal(true)
+        })
+
+        it("should return false when a tag is not present", () => {
+            const context = plan(new MetadataReader(), container, false, TargetTypeEnum.Variable,
+                "Weapon", [["throwable", false], ["spikes", 5]])
+
+            const constraint = multiTaggedConstraint(["throwable", false], ["spikes", 4]);
+            const constraintResult = constraint(context.plan.rootRequest)
+
+            expect(constraintResult).to.equal(false)
+        })
+    })
 });
