d72e6bae564542b20d603ca5cce26e48cfeb33af
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-11-02 13:19:41 +0100

    refactor: remove ServiceIdentifierOrFunc

--- src/annotation/lazy_service_identifier.ts
+++
@@ -1,7 +0,0 @@
-import { LazyServiceIdentifier } from '@inversifyjs/common';
-
-import { interfaces } from '../interfaces/interfaces';
-
-export type ServiceIdentifierOrFunc<T> =
-  | interfaces.ServiceIdentifier<T>
-  | LazyServiceIdentifier<T>;
