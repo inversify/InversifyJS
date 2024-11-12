00cb0ba3cf841c5d794e4b949e2671697a21a221
notaphplover <roberto.pintos.lopez@gmail.com>
2024-11-12 12:54:57 +0100

    Add missing typescript esm types (#1623)
    * chore: add writeEsmPackageJson script
    
    * chore: update builds to emit type declaration files
    
    * docs: update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 
 ### Fixed
+- Updated ESM build with missing types.
 
 ## [6.1.2]
 
---
+++ scripts/writeEsmPackageJson.mjs
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+import fs from 'node:fs/promises';
+import { argv } from 'node:process';
+import path from 'node:path';
+import { writeFile } from 'node:fs/promises';
+
+/**
+ * @param {string} path
+ * @returns {Promise<boolean>}
+ */
+async function pathExists(path) {
+  try {
+    await fs.access(path);
+    return true;
+  } catch (_err) {
+    return false;
+  }
+}
+
+const directory = argv[2];
+
+if (directory === undefined) {
+  throw new Error('Expected a path');
+}
+
+const directoryExists = await pathExists(directory);
+
+if (!directoryExists) {
+  throw new Error(`Path ${directory} not found`);
+}
+
+const filePath = path.join(directory, 'package.json');
+
+const packageJsonFileContent = JSON.stringify(
+  {
+    type: 'module',
+  },
+  undefined,
+  2,
+);
+
+await writeFile(filePath, packageJsonFileContent);
