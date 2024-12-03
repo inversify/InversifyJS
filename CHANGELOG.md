d9369ee0a329f4dbb6512629c3ed8f60dd60c693
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-12-03 16:56:06 +0100

    fix: update getBaseClassDependencyCount to rely on getBaseType

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 
 ### Fixed
+- Fixed unexpected property access while running planning checks on injected base types.
 
 ## [6.1.5]
 
--- src/planning/reflection_utils.ts
+++ src/planning/reflection_utils.ts
@@ -3,6 +3,7 @@ import { getTargets } from '@inversifyjs/core';
 
 import * as METADATA_KEY from '../constants/metadata_keys';
 import { interfaces } from '../interfaces/interfaces';
+import { getBaseType } from '../utils/get_base_type';
 import { getFunctionName } from '../utils/serialization';
 import { Metadata } from './metadata';
 
@@ -17,39 +18,36 @@ function getBaseClassDependencyCount(
   metadataReader: interfaces.MetadataReader,
   func: NewableFunction,
 ): number {
-  const baseConstructor: NewableFunction =
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    Object.getPrototypeOf(func.prototype).constructor as NewableFunction;
-  if (baseConstructor !== (Object as unknown as NewableFunction)) {
-    // get targets for base class
+  const baseConstructor: Newable | undefined = getBaseType(func);
 
-    const targets: interfaces.Target[] = getTargets(metadataReader)(
-      baseConstructor as Newable,
-    );
-
-    // get unmanaged metadata
-    const metadata: interfaces.Metadata[][] = targets.map(
-      (t: interfaces.Target) =>
-        t.metadata.filter(
-          (m: interfaces.Metadata) => m.key === METADATA_KEY.UNMANAGED_TAG,
-        ),
-    );
-
-    // Compare the number of constructor arguments with the number of
-    // unmanaged dependencies unmanaged dependencies are not required
-    const unmanagedCount: number = ([] as Metadata[]).concat.apply(
-      [],
-      metadata,
-    ).length;
-    const dependencyCount: number = targets.length - unmanagedCount;
-
-    if (dependencyCount > 0) {
-      return dependencyCount;
-    } else {
-      return getBaseClassDependencyCount(metadataReader, baseConstructor);
-    }
-  } else {
+  if (baseConstructor === undefined || baseConstructor === Object) {
     return 0;
+  }
+
+  // get targets for base class
+  const targets: interfaces.Target[] =
+    getTargets(metadataReader)(baseConstructor);
+
+  // get unmanaged metadata
+  const metadata: interfaces.Metadata[][] = targets.map(
+    (t: interfaces.Target) =>
+      t.metadata.filter(
+        (m: interfaces.Metadata) => m.key === METADATA_KEY.UNMANAGED_TAG,
+      ),
+  );
+
+  // Compare the number of constructor arguments with the number of
+  // unmanaged dependencies unmanaged dependencies are not required
+  const unmanagedCount: number = ([] as Metadata[]).concat.apply(
+    [],
+    metadata,
+  ).length;
+  const dependencyCount: number = targets.length - unmanagedCount;
+
+  if (dependencyCount > 0) {
+    return dependencyCount;
+  } else {
+    return getBaseClassDependencyCount(metadataReader, baseConstructor);
   }
 }
 
